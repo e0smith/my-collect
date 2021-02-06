9a81c756bf0d8cfbff0c1f1f1957bcc6ce5d3822
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-02-06 11:22:00 -0500

    commit tree 88d329d20a796d1f56107289930e0534fb6a8d7b

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+#hopefully third times the charm of deleting and creating a new directory
+#for this lab to submit it
 
-
+require 'pry'
+def my_collect(names)
+    #binding.pry
+    c = 0 #counter
+    i = [] #new array
+    while c < names.length 
+        i << yield(names[c])
+        c += 1
+    end
+    i
+end
